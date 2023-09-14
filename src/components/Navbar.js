262a1f311b9c7bed4d1a94c2ee0592369663125e
GBRbotelho <gabrielbbotelho05@gmail.com>
2023-09-14 19:55:35 -0300

    Estrutura

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  const [count, setCount] = useState(0);
+
+  return <nav className={styles.nav}>
+    
+  </nav>;
+}
+
+export default Navbar;
