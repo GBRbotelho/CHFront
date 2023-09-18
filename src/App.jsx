a83a48261583faedf2eaf7f1e55cc0affbb7e2b6
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2023-09-18 18:06:07 -0300

    Container #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Container1 from "./containers/Container1";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,9 @@ function App() {
   return (
     <main>
       <Navbar />
+      <div className="Conteudo">
+        <Container1 />
+      </div>
     </main>
   );
 }
---
+++ src/containers/Container1.jsx
@@ -0,0 +1,22 @@
+import styles from "./Container1.module.css";
+
+function Container1() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h2>BUREAU</h2>
+        <h3>DE IMPRESSÂO</h3>
+        <p>
+          7 anos no mercado entregando com rapidez e qualidade, grandes
+          <br />
+          produções.Com a estrutura para realizar{" "}
+          <span className={styles.caneta}>qualquer projeto</span>
+        </p>
+        <button>Entre em Contato</button>
+      </div>
+      <div className={styles.divImage}></div>
+    </div>
+  );
+}
+
+export default Container1;
