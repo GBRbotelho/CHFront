ec6c9ddd5476b16db950a1b399ae4c116af3f4d3
GBRbotelho <gabrielbbotelho05@gmail.com>
2023-09-20 19:14:19 -0300

    Correções

--- src/containers/Container1.jsx
+++ src/containers/Container1.jsx
@@ -4,15 +4,15 @@ function Container1() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h2>BUREAU</h2>
-        <h3>DE IMPRESSÃO</h3>
-        <p>
+        <h2 className={styles.h2}>BUREAU<br/>
+        <span>DE IMPRESSÃO</span></h2>
+        <p className={styles.p}>
           7 anos no mercado entregando com rapidez e qualidade, grandes
           <br />
           produções.Com a estrutura para realizar{" "}
           <span className={styles.caneta}>qualquer projeto</span>
         </p>
-        <button>Entre em Contato</button>
+        <button>Entre em contato</button>
       </div>
       <div className={styles.divImage}></div>
     </div>
