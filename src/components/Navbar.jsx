2b784bd1a03a1e00326f9f2f1f33d5953eb3c294
GBRbotelho <gabrielbbotelho05@gmail.com>
2023-09-20 17:22:43 -0300

    Navbar efeito e sombra

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,36 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
 import Logo from "../images/Logo.png";
 
 function Navbar() {
-  const [count, setCount] = useState(0);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos, visible]);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+
+    // Limita o desaparecimento da barra de navegação após rolar 120px
+    const scrollThreshold = 55;
+
+    if (currentScrollPos < scrollThreshold) {
+      setVisible(true); // Mantém a barra de navegação visível
+    } else {
+      const visible = prevScrollPos > currentScrollPos;
+      setVisible(visible);
+    }
+
+    setPrevScrollPos(currentScrollPos);
+  };
 
   return (
-    <nav className={styles.nav}>
+    <nav className={`${styles.nav} ${visible ? styles.visible : styles.hidden}`}>
       <div className={styles.divLogo}>
         <img src={Logo} alt="Descrição da imagem" />
       </div>
