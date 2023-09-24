b6ce5d7aae49e42c1c9c910c0797f7924d54a89b
GBRbotelho <gabrielbbotelho05@gmail.com>
2023-09-24 14:26:40 -0300

    Container 5

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Container1 from "./containers/Container1";
 import Container2 from "./containers/Container2";
 import Container3 from "./containers/Container3";
 import Container4 from "./containers/Container4";
+import Container5 from "./containers/Container5";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,6 +18,7 @@ function App() {
         <Container2 />
         <Container3 />
         <Container4 />
+        <Container5 />
       </div>
     </main>
   );
