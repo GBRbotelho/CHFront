58352a0d91235b3cd49660a9ec0f14d5ac0663a4
GBRbotelho <gabrielbbotelho05@gmail.com>
2023-09-24 20:47:59 -0300

    Container 7

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Container3 from "./containers/Container3";
 import Container4 from "./containers/Container4";
 import Container5 from "./containers/Container5";
 import Container6 from "./containers/Container6";
+import Container7 from "./containers/Container7";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,6 +23,7 @@ function App() {
         <Container4 />
         <Container5 />
         <Container6 />
+        <Container7 />
       </div>
     </main>
   );
---
+++ src/containers/Container7.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./Container7.module.css";
+import Logo from "../images/Logo.png"
+import IconInsta from "../images/iconinsta.svg"
+import IconWhats from "../images/iconwhats.svg"
+
+function Container7() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <img src={Logo} alt="" />
+        <div className={styles.text}>
+            <div className={styles.text1}>
+                <div className={styles.textContent1}>
+                    <h3>Horário de funcionamento</h3>
+                    <p>Seg a Sex - 08:00 ás 18:00</p>
+                </div>
+                <div className={styles.textContent2}>
+                    <h3>Endereço</h3>
+                    <p>Rua Argemiro Piva, 330 - Vila Jose Paulino Nogueira, Paulínia - SP 13140-426</p>
+                </div>
+            </div>
+            <div className={styles.text2}>
+                <h3>Acompanhe nas redes sociais</h3>
+                <div className={styles.icon}>
+                    <img src={IconInsta} alt="" />
+                    <p>@xxxxx</p>
+                </div>
+                <div className={styles.icon1}>
+                    <img src={IconWhats} alt="" />
+                    <p>@xxxxx</p>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Container7;
+
