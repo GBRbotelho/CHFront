737ce56cc7737b381737a5a10d75b6136a40e720
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2023-11-25 20:02:40 -0300

    Small Devices Container 3, 5, 6, 7

--- src/containers/Container3.jsx
+++ src/containers/Container3.jsx
@@ -1,37 +1,45 @@
 import styles from "./Container3.module.css";
-import Marca from "../images/Marca.svg"
+import Marca from "../images/Logo.png";
 
 function Container3() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.content1}>
-            <div className={styles.div1}>
-              <h2>O QUE<br/>OS<br/><span>CLIENTES</span><br/>DIZEM</h2>
-            </div>
-            <div className={styles.div2}>
-              <section ></section>
-            </div>
+          <div className={styles.div1}>
+            <h2>
+              O QUE
+              <br />
+              OS
+              <br />
+              <span>CLIENTES</span>
+              <br />
+              DIZEM
+            </h2>
+          </div>
+          <div className={styles.div2}>
+            <section></section>
+          </div>
         </div>
-        <div className={styles.content2}>
-            <ul>
-                <li>
-                    <img src={Marca}/>
-                </li>
-                <li>
-                    <img src={Marca}/>
-                </li>
-                <li>
-                    <img src={Marca}/>
-                </li>
-                <li>
-                    <img src={Marca}/>
-                </li>
-                <li>
-                    <img src={Marca}/>
-                </li>
-            </ul>
-        </div>
+        {/* <div className={styles.content2}>
+          <ul>
+            <li>
+              <img src={Marca} />
+            </li>
+            <li>
+              <img src={Marca} />
+            </li>
+            <li>
+              <img src={Marca} />
+            </li>
+            <li>
+              <img src={Marca} />
+            </li>
+            <li>
+              <img src={Marca} />
+            </li>
+          </ul>
+        </div> */}
       </div>
     </div>
   );
