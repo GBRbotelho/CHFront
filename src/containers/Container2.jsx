49ceb3f7d4716b7fe4bac8cf5aa7ce239a475aaa
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2023-11-26 16:10:38 -0300

    Container 2

--- src/containers/Container2.jsx
+++ src/containers/Container2.jsx
@@ -1,4 +1,7 @@
 import styles from "./Container2.module.css";
+import Truck from "../images/truck.svg";
+import Banner from "../images/banner.svg";
+import Megaphone from "../images/megaphone.svg";
 
 function Container2() {
   return (
@@ -14,11 +17,35 @@ function Container2() {
         </div>
         <div className={styles.cardsContainer}>
           <div className={styles.card1}>
-            <div></div>
-            <div></div>
+            <div className={styles.card}>
+              <div className={styles.cardContainer}>
+                <div className={styles.line}></div>
+                <div className={styles.cardText}>
+                  <img src={Megaphone}></img>
+                  <h2>GRANDES FORMATOS</h2>
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardContainer}>
+                <div className={styles.line}></div>
+                <div className={styles.cardText}>
+                  <img src={Banner}></img>
+                  <h2>LONAS, BANNER´S</h2>
+                </div>
+              </div>
+            </div>
           </div>
           <div className={styles.card2}>
-            <section></section>
+            <section className={styles.card}>
+              <div className={styles.cardContainer}>
+                <div className={styles.line}></div>
+                <div className={styles.cardText}>
+                  <img src={Truck}></img>
+                  <h2>ADESIVOS, ENVELOPAMENTO</h2>
+                </div>
+              </div>
+            </section>
             <div className={styles.text2}>
               <h2>
                 PROCURANDO
