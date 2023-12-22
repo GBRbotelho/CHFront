9b0a751a8808b698cc536802959d73964e2c592b
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2023-12-21 21:28:43 -0300

    Load Image

--- src/containers/ModalPopUp.jsx
+++ src/containers/ModalPopUp.jsx
@@ -7,15 +7,20 @@ import popupMobile from "../images/popupMobile.png";
 export default function ModalPopUp() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   const handleWindowResize = () => {
     setWindowWidth(window.innerWidth);
   };
 
-  //Estilos
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
+  // Estilos
   const modalStyles = {
     content: {
-      zIndex: 9999, // Ajuste o valor conforme necessário (use um valor maior)
+      zIndex: 9999,
       width: "min-content",
       height: "min-content",
       margin: "auto",
@@ -27,25 +32,26 @@ export default function ModalPopUp() {
     },
     overlay: {
       backgroundColor: "rgba(0, 0, 0, 0.5)",
-      zIndex: 999800000, // Adicione zIndex ao overlay se necessário
+      zIndex: 999800000,
     },
   };
 
   useEffect(() => {
-    // Adiciona um ouvinte de evento para redimensionamento da janela
     window.addEventListener("resize", handleWindowResize);
 
-    // Abre o modal após 2 segundos
-    const timeoutId = setTimeout(() => {
+    // Pode iniciar o carregamento da imagem imediatamente
+    const image = new Image();
+    image.src = windowWidth > 830 ? popup : popupMobile;
+    image.onload = () => {
+      setImageLoaded(true);
       setIsModalOpen(true);
-    }, 2000);
+    };
 
-    // Limpa o timeout e remove o ouvinte de evento quando o componente é desmontado
+    // Limpa o ouvinte de evento quando o componente é desmontado
     return () => {
-      clearTimeout(timeoutId);
       window.removeEventListener("resize", handleWindowResize);
     };
-  }, []);
+  }, [windowWidth]);
 
   const whatsappText = encodeURIComponent("Quero aproveitar a promoção");
 
@@ -57,20 +63,31 @@ export default function ModalPopUp() {
       style={modalStyles}
     >
       <div className={styles.content}>
-        <img src={windowWidth > 830 ? popup : popupMobile} />
-        <button className={styles.close} onClick={() => setIsModalOpen(false)}>
-          <i class="ri-close-fill"></i>
-        </button>
-        <div className={styles.buttonDiv}>
-          <a
-            href={`https://wa.me/19974026227?text=${whatsappText}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className={styles.button}>Aproveitar agora</button>
-          </a>
-          <p onClick={() => setIsModalOpen(false)}>não quero economizar</p>
-        </div>
+        {imageLoaded && (
+          <>
+            <img
+              src={windowWidth > 830 ? popup : popupMobile}
+              onLoad={handleImageLoad}
+              alt="Promoção"
+            />
+            <button
+              className={styles.close}
+              onClick={() => setIsModalOpen(false)}
+            >
+              <i className="ri-close-fill"></i>
+            </button>
+            <div className={styles.buttonDiv}>
+              <a
+                href={`https://wa.me/19974026227?text=${whatsappText}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className={styles.button}>Aproveitar agora</button>
+              </a>
+              <p onClick={() => setIsModalOpen(false)}>não quero economizar</p>
+            </div>
+          </>
+        )}
       </div>
     </Modal>
   );
